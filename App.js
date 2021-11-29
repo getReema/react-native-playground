488a46599ded83d6318d3139fa7125584dbb5c73
getReema <reeroo02@gmail.com>
2021-11-29 14:32:53 +0300

    Text and Prees one

--- App.js
+++ App.js
@@ -5,7 +5,11 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines= {3} onPress={()=> console.log('Text Pressed')}> Hello Open up App.js to start working on your app!
+      Open up App.js to start working on your app!
+      Open up App.js to start working on your app!
+      Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
