47651786fbf27fb5042ebe5cf1a45c12ed138867
getReema <reeroo02@gmail.com>
2021-11-29 15:04:23 +0300

    Implement Touchable for Image

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
   
-
-        <Text> Hello React Native </Text>
+<TouchableOpacity onPress={()=> console.log('Image Pressed')}>
         <Image source={{
           uri: 'https://saib.com.sa/sites/default/files/logo.png',
           width:412,
           height:122
         }} />
-
+</TouchableOpacity>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
